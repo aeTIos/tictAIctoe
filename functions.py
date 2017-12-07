c9c37007e8389c7cb9fdbc4fa349479f12fc8305
ben <b.schumann1987@gmail.com>
2017-12-07 14:44:13 +0100

    added validmoves function

--- functions.py
+++ functions.py
@@ -2,6 +2,16 @@ import hashlib
 
 #Ubersimple hashing function
 def hash(array):
-	strarray = str(array)
-	input = ''.join(strarray)
-	return hashlib.sha256(input.encode('utf-8')).hexdigest()
+    strarray = str(array)
+    input = ''.join(strarray)
+    return hashlib.sha256(input.encode('utf-8')).hexdigest()
+
+def getvalidmoves(gamestate):
+    availablemoves = []
+    for y in range(len(gamestate)):
+        for x in range(len(gamestate[y])):
+            if gamestate[y][x] == 0:
+                coord = (y,x)
+                availablemoves.append(coord)
+    return availablemoves
+
