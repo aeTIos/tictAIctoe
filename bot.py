dec5f5371365fd00c63150431426fc3520141ad6
ben <b.schumann1987@gmail.com>
2017-12-07 13:51:59 +0100

    cleaned up bot.py

--- bot.py
+++ bot.py
@@ -1,55 +1,16 @@
 from copy import deepcopy
 
 
-#def generate_moves(gamestates):
-#    states = []
-#    # generate_moves returns an array of moves
-#    for state in gamestates: # state is a 2 dimensional array
-#        print(state)
-#        for i in range(0, len(state)):
-#            for j in range(0, len(state)):
-#                if state[i][j] == 0:
-#                    placeholder = state
-#                    placeholder[i][j] = 1
-#
-#                    print('\n\n', placeholder, '\n', state)
-#                    states.append(placeholder)
-#
-#    return states
-#
-#
-#round_0 = [[[0, 0],
-#           [0, 0]]]
-#
-#round_1 = generate_moves(round_0)
-##round_2 = generate_moves(round_1)
-##round_3 = generate_moves(round_2)
-#
-#print(round_1)
-#
-
-
 def generate_boards(size):
 
     array = [[0 for i in range(size)] for i in range(0, size)]
 
-    boards = []
+    boards = [[[0, 0, 0], [0, 0, 0], [0, 0, 0]]]
 
     get_states(size, deepcopy(array), boards, 1, 0)
 
     print(f'\n\n\n{boards}')
     print(len(boards))
-
-#
-#def generate_moves(gamestates): # gamestates is a list of.. gamestates. v
-#    # generate_moves returns an array of moves
-#    for state in gamestates: # state is a 2 dimensional array
-#        for i in range(0, len(state)):
-#            for j in range(0, len(state)):
-#
-#
-#    # generate all possible next moves from this array
-#
 
 
 def win(array):
@@ -93,37 +54,30 @@ def get_states(size, array, boards, player, iteration):
 
 
 
-  #              print(f'\n\nTempboard at start of iteration: {tempboard}\n iter {i} {j}. Step: {iteration}')
- #               print(f'We are going to try position {i}, {j}')
+                #              print(f'\n\nTempboard at start of iteration: {tempboard}\n iter {i} {j}. Step: {iteration}')
+                #               print(f'We are going to try position {i}, {j}')
 
                 if tempboard[i][j] == 0:
-#                    print('this is a valid move!')
+                    #                    print('this is a valid move!')
 
                     tempboard[i][j] = iteration % 2 + 1
 
-  #                  print(f'tempboard is now: \n {tempboard}')
- #                   print(f'The existing boards are: \n {boards}')
+                    #                  print(f'tempboard is now: \n {tempboard}')
+                    #                   print(f'The existing boards are: \n {boards}')
 
                     if tempboard not in boards:
 
-                        #print('this is a new state!')
+                        # print('this is a new state!')
 
                         boards.append(tempboard)
-                        #print('appended tempboard to boards!')
-                        #print(f'Boards is now:\n {boards}')
+                        # print('appended tempboard to boards!')
+                        # print(f'Boards is now:\n {boards}')
                         get_states(size, deepcopy(tempboard), boards, player, iteration + 1)
                     else:
                         pass
-                        #print('This state already exists!')
+                        # print('This state already exists!')
                 else:
                     pass
-                    #print('this is not a valid move!')
-
-                #print(f'trying next state... {iteration}')
-    else:
-        print(f'We\'ve got a win condition!\n {array} \n The winner is player {winyn}\n')
-
-
 
 #testarray = [[1,0,2],
 #             [0,2,0],
@@ -132,9 +86,3 @@ def get_states(size, array, boards, player, iteration):
 #print(win(testarray))
 
 generate_boards(3)
-
-
-
-
-
-
