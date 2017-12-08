6a29c5f79480c92460e83334ba933fc426ceca7c
Richard van Dijk <dijkvanrichard@gmail.com>
2017-12-08 14:46:55 +0100

    Major refactor. *Moved functions from bot and hashgenerator to functions.py. *Started writing main function in bot.py

--- bot.py
+++ bot.py
@@ -1,85 +1,17 @@
-from copy import deepcopy
+import functions
+
+#initialise
+big_db = functions.load_json('db.json')
+movesduringgame = {}
 
 
-def generate_boards(size):
+def main(board, player):
+    boardhash = functions.gamestate_hash(board)
+    possiblemoves = functions.fetch_moves(big_db, boardhash)
+    move = functions.generate_move(possiblemoves)
+    board = functions.update_board(board, move)
+    endgame = functions.game_end(board)
+    if endgame in [1,2]:
+        if endgame == player:
+            # WE WIN!!!!11!!!!
 
-    array = [[0 for i in range(size)] for i in range(0, size)]
-
-    boards = [[[0, 0, 0], [0, 0, 0], [0, 0, 0]]]
-
-    get_states(size, deepcopy(array), boards, 1, 0)
-
-    return boards
-
-
-def win(array):
-    length = len(array)
-    vals = [1, 2]
-
-    for player in vals:
-        diag1 = 0
-        diag2 = 0
-
-        for i in range(0, length):
-            dir1 = 0
-            dir2 = 0
-            for j in range(0, length):
-                if array[j][i] == player:
-                    dir1 += 1
-
-                if array[i][j] == player:
-                    dir2 += 1
-
-                if dir1 == 3 or dir2 == 3:
-                    return player
-
-            if array[i][i] == player:
-                diag1 += 1
-            if array[i][length - 1 - i] == player:
-                diag2 += 1
-            if diag1 == 3 or diag2 == 3:
-                return player
-
-    return False
-
-def get_states(size, array, boards, player, iteration):
-    winyn = win(array)
-    if winyn is False:
-        for i in range(0, size):
-            for j in range(0, size):
-
-
-                tempboard = deepcopy(array)
-
-
-
-                #              print(f'\n\nTempboard at start of iteration: {tempboard}\n iter {i} {j}. Step: {iteration}')
-                #               print(f'We are going to try position {i}, {j}')
-
-                if tempboard[i][j] == 0:
-                    #                    print('this is a valid move!')
-
-                    tempboard[i][j] = iteration % 2 + 1
-
-                    #                  print(f'tempboard is now: \n {tempboard}')
-                    #                   print(f'The existing boards are: \n {boards}')
-
-                    if tempboard not in boards:
-
-                        # print('this is a new state!')
-
-                        boards.append(tempboard)
-                        # print('appended tempboard to boards!')
-                        # print(f'Boards is now:\n {boards}')
-                        get_states(size, deepcopy(tempboard), boards, player, iteration + 1)
-                    else:
-                        pass
-                        # print('This state already exists!')
-                else:
-                    pass
-
-#testarray = [[1,0,2],
-#             [0,2,0],
-#             [2,1,1]]
-
-#print(win(testarray))
--- functions.py
+++ functions.py
@@ -1,28 +1,113 @@
 import hashlib
 import json
+from copy import deepcopy
 
-#Ubersimple hashing function
-def hash(array):
+
+# Generate sha256 hash of gamestate
+def gamestate_hash(array):
     strarray = str(array)
-    input = ''.join(strarray)
-    return hashlib.sha256(input.encode('utf-8')).hexdigest()
+    return hashlib.sha256(strarray.encode('utf-8')).hexdigest()
 
-def getvalidmoves(gamestate):
+
+# return array with all possible moves
+def get_valid_moves(gamestate):
     availablemoves = []
     for y in range(len(gamestate)):
         for x in range(len(gamestate[y])):
             if gamestate[y][x] == 0:
-                coord = (y,x)
+                coord = (y, x)
                 availablemoves.append(coord)
     return availablemoves
 
-def loadjson(filename):
+
+
+
+
+
+# generate a dictionary {HASH: moves} based on an array with gamestates
+def generate_database(states):
+    dictionary = {}
+    for state in states:
+        dictionary.update({gamestate_hash(state): get_valid_moves(state)})
+    return dictionary
+
+
+# return False if there is no win condition; else return winning player
+def game_end(array):
+    length = len(array)
+    vals = [1, 2]
+    for player in vals:
+        diag1 = 0
+        diag2 = 0
+        for i in range(0, length):
+            dir1 = 0
+            dir2 = 0
+            for j in range(0, length):
+                if array[j][i] == player:
+                    dir1 += 1
+                if array[i][j] == player:
+                    dir2 += 1
+                if dir1 == 3 or dir2 == 3:
+                    return player
+            if array[i][i] == player:
+                diag1 += 1
+            if array[i][length - 1 - i] == player:
+                diag2 += 1
+            if diag1 == 3 or diag2 == 3:
+                return player
+    return False
+
+
+# loads json file and return a dictionary
+def load_json(filename):
     with open(filename, 'r') as f:
         jsonfile = json.load(f)
         return jsonfile
 
-def savejson(filename, dictionary):
+
+# take a dictionary and save it as a json file
+def save_json(filename, dictionary):
     with open(filename, 'w') as f:
         json.dump(dictionary, f)
-    
 
+
+# generate all games on a size x size board
+def generate_boards(size):
+    array = [[0 for i in range(size)] for i in range(0, size)]
+    boards = [deepcopy(array)]
+    get_gamestates(size, deepcopy(array), boards, 1, 0)
+    return boards
+
+
+# Generate all possible boards based on an initial state
+def get_gamestates(size, gamestate, boards, player, iteration):
+    winyn = game_end(gamestate)
+    if winyn is False:
+        for i in range(0, size):
+            for j in range(0, size):
+                tempboard = deepcopy(gamestate)
+                if tempboard[i][j] == 0:
+                    tempboard[i][j] = iteration % 2 + 1
+                    if tempboard not in boards:
+                        boards.append(tempboard)
+                        get_gamestates(size, deepcopy(tempboard), boards, player, iteration + 1)
+
+
+
+# fetch array with move statistics from database
+def fetch_moves(db, hash):
+    pass # return array [[y,x],[y,x]...[100, 200...]]
+
+
+# generate a move based on the possible moves
+def generate_move(moves):
+    pass # return a tuple (y, x)
+
+
+#update a board array with a move
+def update_board(board, move)
+    pass # return new board
+
+# update the database for played moves with new values by amount
+def update_database(moves, database, amount):
+    pass
