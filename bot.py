9a9365f8058f10b9d841d3f75f62904b6b34fe53
Richard van Dijk <dijkvanrichard@gmail.com>
2017-12-14 15:37:31 +0100

    rename main() to move(). Remove some code

--- bot.py
+++ bot.py
@@ -1,17 +1,33 @@
 import functions
 
 #initialise
-big_db = functions.load_json('db.json')
-movesduringgame = {}
+class Vars:
+    def __init__(self):
+        self.big_db = functions.load_json('db.json')
+        self.movesduringgame = {}
 
 
-def main(board, player):
+
+
+def move(board, player):
     boardhash = functions.gamestate_hash(board)
     possiblemoves = functions.fetch_moves(big_db, boardhash)
     move = functions.generate_move(possiblemoves)
+    movesduringgame.update({boardhash: move})
     board = functions.update_board(board, move)
     endgame = functions.game_end(board)
-    if endgame in [1,2]:
-        if endgame == player:
-            # WE WIN!!!!11!!!!
 
+    if endgame in [0,1,2]:
+        if endgame == 0:
+            # phew, a draw
+            adjust = 5
+        elif endgame == player:
+            # WE WIN!!!!11!!!!
+            adjust = 10
+        else:
+            # WE LOST :((((((
+            adjust = -1
+        functions.update_database(movesduringgame, big_db, adjust)
+        movesduringgame = {} #reset array of played moves
+
+    return move
