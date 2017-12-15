d9b81a87a900e74f867473a873b6548c9caa6fd6
Richard van Dijk <dijkvanrichard@gmail.com>
2017-12-15 17:21:10 +0100

    Add more functions. * game_end: detect draws * generate_database, init_valid_moves: add num_moves parameter * add invert_board and free_fields functions * turned off print statements

--- functions.py
+++ functions.py
@@ -10,21 +10,25 @@ def gamestate_hash(array):
 
 
 # return array with all possible moves
-def init_valid_moves(gamestate):
+def init_valid_moves(gamestate, num_moves):
     availablemoves = []
     for y in range(len(gamestate)):
         for x in range(len(gamestate[y])):
             if gamestate[y][x] == 0:
-                coord = [(y, x), 100]
+                coord = [y, x]
                 availablemoves.append(coord)
+
+    availablemoves.append([])
+    for i in range(len(availablemoves)-1):
+        availablemoves[-1].append(num_moves)
     return availablemoves
 
 
 # generate a dictionary {HASH: moves} based on an array with gamestates
-def generate_database(states):
+def generate_database(states, num_moves):
     dictionary = {}
     for state in states:
-        dictionary.update({gamestate_hash(state): init_valid_moves(state)})
+        dictionary.update({gamestate_hash(state): init_valid_moves(state, num_moves)})
     return dictionary
 
 
@@ -51,6 +55,16 @@ def game_end(array):
                 diag2 += 1
             if diag1 == 3 or diag2 == 3:
                 return player
+    #detect a draw
+    count = 0
+    for x in range(0, length):
+        for y in range(0, length):
+            if array[y][x] in [1, 2]:
+                count += 1
+    if count == 9:
+        return 3
+
+
     return False
 
 
@@ -63,7 +77,7 @@ def load_json(filename):
 
 # take a dictionary and save it as a json file
 def save_json(filename, dictionary):
-    with open(filename, 'w') as f:
+    with open(filename, 'w+') as f:
         json.dump(dictionary, f)
 
 
@@ -75,6 +89,7 @@ def generate_boards(size):
     array = [[0 for i in range(size)] for i in range(0, size)]
     boards = [deepcopy(array)]
     get_gamestates(size, deepcopy(array), boards, 1, 0)
+    # print(len(boards))
     return boards
 
 
@@ -96,19 +111,20 @@ def get_gamestates(size, gamestate, boards, player, iteration):
 
 # fetch array with move statistics from database
 def fetch_moves(database, boardhash):
-    with open(database, 'r') as f:
-        workfile = json.load(f)
-        return workfile.get(boardhash)
+    #with open(database, 'r') as f:
+    #    workfile = json.load(f)
+    return database.get(boardhash)
 
 
 # generate a move based on the possible moves
 def generate_move(inputarray):
+    ## print('<GM>' + str(inputarray))
     numberarray = inputarray[-1]
     combined_number = 0
     encounterzero = []
 
     for i in range(0,len(numberarray)):
-        if numberarray[i] is 0:
+        if numberarray[i] <= 0:
             encounterzero.append(i)
         combined_number += numberarray[i]
 
@@ -134,12 +150,33 @@ def update_database(moves, database, amount):
         avail_moves = database[boardhash]
         # find the move that we played
         played_move = moves[boardhash]
+        # print('<UPD_DB> '+str(played_move))
         # convert this tuple to an array
         played_move = [x for x in played_move]
-        print(played_move)
+        # print('<UPD_DB> '+str(played_move))
         # match this move with the available moves to see which we need to update
-        for i in avail_moves:
-            print(i)
-            if i[0] == played_move:
-                i[1] += amount
-        return
+        for i in range(len(avail_moves)-1):
+           # # print('<UPD_DB> '+ str(avail_moves[i]))
+            if avail_moves[i] == played_move:
+                avail_moves[-1][i] += amount
+
+                if avail_moves[-1][i] <= 0:
+                    avail_moves[-1][i] = 0
+        # print('<UPD_DB> '+str(avail_moves))
+
+    return
+
+# invert the board because we're dorks
+def invert_board(board):
+    for x in range(len(board)):
+        for y in range(len(board[x])):
+            if board[x][y] in [1,2]:
+                board[x][y] = 1 if board[x][y] is 2 else 2
+    return board
+
+
+#get free fields
+def free_fields(array):
+    strarray = str(array)
+    emptyfields = strarray.count("0")
+    return emptyfields
